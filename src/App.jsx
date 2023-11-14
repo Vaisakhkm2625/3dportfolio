88ec758e30d6f255c4090e26c24069cc2da027fd
Vaisakh K M <vaisakhkm2625@gmail.com>
2023-11-14 21:48:43 +0530

    removed tech part

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,9 @@ const App = () => {
                 </div>
                 <About />
                 <Experience />
-                <Tech />
+                {
+                    //<Tech />
+                }
                 <Works />
                 <Feedbacks />
                 <div className="relative z-0">
