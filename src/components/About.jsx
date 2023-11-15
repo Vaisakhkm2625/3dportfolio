6637ebeb6ae3e588b934ff4692c90dfb96416d2d
Vaisakh K M <vaisakhkm2625@gmail.com>
2023-11-15 18:11:53 +0530

    added overview

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,13 @@ const About = () => {
                 variants={fadeIn("", "", 0.1, 1)}
                 className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
             >
-                I'm a skilled software developer with experience in TypeScript and
-                JavaScript, and expertise in frameworks like React, Node.js, and
-                Three.js. I'm a quick learner and collaborate closely with clients to
-                create efficient, scalable, and user-friendly solutions that solve
-                real-world problems. Let's work together to bring your ideas to life!
+                I am a skilled software developer with expertise in web frameworks
+                like Next.js, Sveltekit, and the Java Spring framework. Additionally,
+                I have experience with Oracle database management and Linux server maintenance.
+                Passionate about Linux and free software development, I am a quick learner and
+                enjoy collaborating with others to create efficient, scalable, and user-friendly
+                solutions that solve real-world problems.
+                Let's work together to bring your ideas to life!
             </motion.p>
 
             <div className='mt-20 flex flex-wrap gap-10'>
